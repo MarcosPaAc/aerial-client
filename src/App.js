a47fc73fe3b2a2cf1908dea8661b1a7cc5bdaf7b
MarcosPaAc <isaelpa09@gmail.com>
2024-05-02 16:47:31 -0400

    setting up connection with API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import api from './api/axiosConfig';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [flights, setData] = useState([]);
+  const getFlights = async () => {
+    
+  try {
+
+    const response = await api.get('/api/v1/flights');
+    
+    console.log(response.data);
+
+    setData(response.data);
+
+  } catch (error) {
+
+    console.log(error);
+  }
+
+  }
+
+  useEffect(() => {
+    getFlights();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
     </div>
   );
 }
